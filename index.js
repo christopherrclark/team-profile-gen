97669bfd641ccbbbd714a3b9d5f2cff3e3ba0ade
Fozzy Yeti <christopherrclark@gmail.com>
2023-01-30 21:47:54 -0600

    fix problem with my code, finish functions in index.js, create page-template.js to generate html and push data to.

--- index.js
+++ index.js
@@ -1,36 +1,92 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const path = require('path');
 
-const Employee = require('./lib/employee');
+const DIST_DIR = path.resolve(__dirname, 'dist');
+const distPath = path.join(DIST_DIR, 'team.html');
+
+const render = require('./src/page-template.js');
+
 const {Manager, questionsManager} = require('./lib/manager');
-const {Engineer,questionsEngineer } = require('./lib/engineer');
-const {Intern,questionsIntern } = require('./lib/intern');
+const {Engineer,questionsEngineer} = require('./lib/engineer');
+const {Intern,questionsIntern} = require('./lib/intern');
 const team = [];
 
 console.log(questionsManager)
 
 function start() {
-  inquirer
-    .prompt(questionsManager)
-    .then((data) => {
-      const manager = new Manager(data.name, data.id, data.email, data.officenumber);
-      team.push(manager)
+  function createManager() {
+    inquirer
+      .prompt(questionsManager)
+      .then((data) => {
+        const manager = new Manager(data.name, data.id, data.email, data.officeNumber);
+        team.push(manager)
+        createTeam()
+        console.log(team)
+      });
 
-      console.log(team)
-    });
+  }
+
+function createTeam() {
+  inquirer.prompt([{
+    type: "list",
+    name: "memberChoice",
+    message: 'Which type of team member would you like to add?',
+          choices: [
+            'Engineer',
+            'Intern',
+            "I don't want to add any more team members",
+          ],
+  }]).then((data) => {
+    switch (data.memberChoice) {
+      case 'Engineer':
+        createEngineer();
+        break;
+      case 'Intern':
+        createIntern();
+        break;
+      default:
+        buildTeam();
+  
+  }})
+
+  function createEngineer() {
+    inquirer
+      .prompt(questionsEngineer)
+      .then((data) => {
+        const engineer = new Engineer(data.name, data.id, data.email, data.github);
+        team.push(engineer)
+        createTeam()
+        
+      });
+
+  }
+
+  function createIntern() {
+    inquirer
+      .prompt(questionsIntern)
+      .then((data) => {
+        const intern = new Intern(data.name, data.id, data.email, data.school);
+        team.push(intern)
+        createTeam()
+        
+      });
+
+  }
+
+}
+createManager()
+
+function buildTeam() {
+  // Create the output directory if the dist path doesn't exist
+  if (!fs.existsSync(DIST_DIR)) {
+    fs.mkdirSync(DIST_DIR);
+  }
+  fs.writeFileSync(distPath, render(team), 'utf-8');
+}
+
 }
 
 
 
 start()
-  // {
-  //   type: 'list',
-  //   message: 'options',
-  //   choices: ["engineer", "intern", "finish building my team"],
-  //   name: 'options',
-  // },
-  
-
-// inquirer
-//   .prompt(questions)  //create this
-//   .then((response) => {})
